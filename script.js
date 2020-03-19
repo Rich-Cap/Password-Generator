aa22f82bef2d34044516b937762b2189bc463a68
Rich Capuy <rccapuy@gmail.com>
2020-03-19 14:10:00 -0400

    added prompts

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+var generateBtn = document.querySelector("#generate");
+
+function writePassword(){
+	alert("Please select criteria to generate a password");
+	prompt("Type a number to choose length of password. Must be between 8 to 128 characters.");
+	prompt("Type 'yes' to include lowercase or press cancel.");
+	prompt("Type 'yes' to include UPPERCASE or press cancel.");
+	prompt("Type 'yes' to include Numbers or press cancel.");
+	prompt("Type 'yes' to include Special Characters or press cancel.");
+	{
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
+
+	passwordText.value = password;
+}
+
+	
+
+}
+
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// 
 var characterAmountNumber = document.getElementById("characterAmountNumber");
 var characterAmountRange = document.getElementById("characterAmountRange");
 
@@ -9,20 +34,3 @@ function syncCharacterAmount(i) {
 	characterAmountNumber.value = value;
 	characterAmountRange.value = value;
 }
-
-
-
-
-
-
-
-var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
-	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
-
-	passwordText.value = password;
-}
-
-generateBtn.addEventListener("click", writePassword);

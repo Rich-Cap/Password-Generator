e8636b35f86a6350be41dcf59dbfdf88ff8f5c44
Rich Capuy <rccapuy@gmail.com>
2020-03-20 18:18:09 -0400

    Figuring out conditionals

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ var generateBtn = document.querySelector("#generate");
 
 function writePassword(){
 	alert("Please select criteria to generate a password");
-	prompt("Type a number to choose length of password. Must be between 8 to 128 characters.");
-	prompt("Type 'yes' to include lowercase or press cancel.");
-	prompt("Type 'yes' to include UPPERCASE or press cancel.");
-	prompt("Type 'yes' to include Numbers or press cancel.");
-	prompt("Type 'yes' to include Special Characters or press cancel.");
+	var number = prompt("Type a number to choose length of password. Must be between 8 to 128 characters.");
+	if (number >= 8 && number <= 128){
+		prompt("Type 'yes' to include lowercase or press cancel.")
+	} else prompt("Type 'yes' to include UPPERCASE or press cancel.");
+	// prompt("Type 'yes' to include UPPERCASE or press cancel.");
+	// prompt("Type 'yes' to include Numbers or press cancel.");
+	// prompt("Type 'yes' to include Special Characters or press cancel.");
 	{
 	var password = generatePassword();
 	var passwordText = document.querySelector("#password");
@@ -18,6 +20,30 @@ function writePassword(){
 
 generateBtn.addEventListener("click", writePassword);
 
+// for (password !== null) {
+// 	// Prompt user to input a letter
+// 	letter = prompt("Type 'h' to honk, 'd' to drive to work, 'w' to drive around the world, or 't' to get a tuneup.");
+
+// 	// If the letter is h, run the following functions/methods.
+// 	if (letter === "h") {
+// 	  car.honk();
+// 	  reWriteStats();
+// 	}
+// 	// If the letter is d, run the following functions/methods.
+// 	else if (letter === "d" && car.isWorking) {
+// 	  car.driveToWork();
+// 	  reWriteStats();
+// 	}
+// 	// If the letter is w, run the following functions/methods.
+// 	else if (letter === "w" && car.isWorking) {
+// 	  car.driveAroundWorld();
+// 	  reWriteStats();
+// 	}
+// 	// If the letter is t, run the following functions/methods.
+// 	else if (letter === "t") {
+// 	  car.getTuneUp();
+// 	  reWriteStats();
+// 	}
 
 
 // Page elements functionality

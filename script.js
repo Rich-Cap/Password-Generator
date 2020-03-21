78d130b80e88450cec716fa936a15aaee429a969
Rich Capuy <rccapuy@gmail.com>
2020-03-21 18:26:42 -0400

    Cleaned up syntax and UI

--- script.js
+++ script.js
@@ -8,29 +8,27 @@ function writePassword(){
 	var getNumber = confirm("Press 'ok' to include Numbers.");
 	var getSymbol = confirm("Press 'ok' to include Special characters.");
 
-	
 	if (input >= 8 && input <= 128){
 		var length = parseInt(input);
 	}else alert("Invalid answer. Try again!");
-	
 	if (getLower === true){
 		var lower = "abcdefghijklmnopqrstuvwxyz";
-	} else lower = ""
+	} else lower = "";
 	if (getUpper === true){
 		var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	} else upper = ""
+	} else upper = "";
 	if (getNumber === true){
 		var numbers = "0123456789";
-	} else numbers = ""
+	} else numbers = "";
 	if (getSymbol === true){
 		var symbol = "!*+,-./:;<=>?{@_`|}~";
-	} else symbol = ""
+	} else symbol = "";
 
-	function makeid(length){
+	function makeid(generate){
 		var result           = "";
         var characters       = lower + upper + numbers + symbol;
         var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
+        for ( var i = 0; i < generate; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
 		return result;
@@ -43,17 +41,3 @@ function writePassword(){
 }
 
 generateBtn.addEventListener("click", writePassword);
-
-//** Page elements functionality - Stopped to redirect purpose of HW assignment */
-
-var characterAmountNumber = document.getElementById("characterAmountNumber");
-var characterAmountRange = document.getElementById("characterAmountRange");
-
-characterAmountRange.addEventListener("input", syncCharacterAmount);
-characterAmountNumber.addEventListener("input", syncCharacterAmount);
-
-function syncCharacterAmount(i) {
-	var value = i.target.value;
-	characterAmountNumber.value = value;
-	characterAmountRange.value = value;
-}

8fda328f26c261a5f052fd1c260b697e137f5a39
Rich Capuy <rccapuy@gmail.com>
2020-03-21 17:37:38 -0400

    changed prompts

--- script.js
+++ script.js
@@ -2,30 +2,31 @@ var generateBtn = document.querySelector("#generate");
 
 function writePassword(){
 	alert("Please select criteria to generate a password");
-	var getLength = prompt("Type a number to choose length of password. Must be between 8 to 128 characters.");
-	var getLower = confirm("Press 'ok' to include lowercase or press cancel.");
-	var getUpper = confirm("Press 'ok' to include UPPERCASE or press cancel.");
-	var getNumber = confirm("Press 'ok' to include Numbers or press cancel.");
-	var getSymbol = confirm("Press 'ok' to include Special Characters or press cancel.");
+	var input = prompt("Type a number to choose length of password. Must be between 8 to 128 characters.");
+	var getLower = confirm("Press 'ok' to include lowercase characters.");
+	var getUpper = confirm("Press 'ok' to include UPPERCASE characters.");
+	var getNumber = confirm("Press 'ok' to include Numbers.");
+	var getSymbol = confirm("Press 'ok' to include Special characters.");
 
 	
-	if (getLength >= 8 && getLength <= 128){
-		length = getLength;
-	}
+	if (input >= 8 && input <= 128){
+		var length = parseInt(input);
+	}else alert("Invalid answer. Try again!");
+	
 	if (getLower === true){
-		getLower = lower;
+		var getLower = lower;
 	}
 	if (getUpper === true){
-		getUpper = upper;
+		var getUpper = upper;
 	}
 	if (getNumber === true){
-		getNumber = number;
+		var getNumber = number;
 	}
 	if (getSymbol === true){
-		getSymbol = symbol;
+		var getSymbol = symbol;
 	}
 
-	function generatePassword(length){
+	function makeid(length){
 		var result           = "";
         var lower            = "abcdefghijklmnopqrstuvwxyz";
         var upper            = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -38,40 +39,13 @@ function writePassword(){
         return result;
 	}
 	
-	var password = generatePassword(length);
+	var password = makeid(length);
 	var passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
 }
 
 generateBtn.addEventListener("click", writePassword);
-
-//** Functions for Options that should be less complicated */
-
-// function writePassword(){
-//     var input = prompt("What number?");
-//     var length = parseInt(input);
-//     function makeid(length) {
-//         var result           = "";
-//         var lower            = "abcdefghijklmnopqrstuvwxyz";
-//         var upper            = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//         var numbers          = "0123456789";
-//         var characters       = lower+upper+numbers;
-//         var charactersLength = characters.length;
-//         for ( var i = 0; i < length; i++ ) {
-//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//         }
-//         return result;
-// }
-// var password = makeid(length);
-// var passwordText = document.querySelector("#password");
-// passwordText.value = password;
-// }
-
-
-
-
-
 
 //** Page elements functionality - Stopped to redirect purpose of HW assignment */
 

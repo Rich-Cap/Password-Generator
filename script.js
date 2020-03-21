b6fcfc645bf6cde958f6d38c73f7622e1f5618cb
Rich Capuy <rccapuy@gmail.com>
2020-03-21 16:15:16 -0400

    Less complicated function to replace

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
 var generateBtn = document.querySelector("#generate");
 
+
+
 function writePassword(){
 	alert("Please select criteria to generate a password");
 	var length = prompt("Type a number to choose length of password. Must be between 8 to 128 characters.");
-	var lower = prompt("Type 'yes' to include lowercase or press cancel.");
-// 	var upper = prompt("Type 'yes' to include UPPERCASE or press cancel.");
-// 	var number = prompt("Type 'yes' to include Numbers or press cancel.");
-// 	var symbol = prompt("Type 'yes' to include Special Characters or press cancel.");
-
-
-// 	function randomLower(){
-// 		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// 	}
-// 	function randomUpper(){
-// 		return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// 	}
-// 	function randomNumber(){
-// 		return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// 	}
-// 	function randomSymbol(){
-// 		var symbols = "!#$%&'*+,-./:;<=>?@\^_`|~"
-// 		return symbols[Math.floor(Math.random() * symbols.length];
-// 	}
-	
+		if (length >= 8){
+			prompt("Type 'yes' to include UPPERCASE or press cancel.");
+		}
+	var randomLower = prompt("Type 'yes' to include lowercase or press cancel.");
+	var upper = prompt("Type 'yes' to include UPPERCASE or press cancel.");
+	var number = prompt("Type 'yes' to include Numbers or press cancel.");
+	var symbol = prompt("Type 'yes' to include Special Characters or press cancel.");
 	{
-	var password = generatePassword();
+	
+	
+	
+		var password = generatePassword();
 	var passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
-}
+	}
 
-}
-
-
-function randomLower(){
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-function randomUpper(){
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function randomNumber(){
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function randomSymbol(){
-	var symbols = "!#$%&'*+,-./:;<=>?@\^_`|~"
-	return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 generateBtn.addEventListener("click", writePassword);
 
+//** Functions for Options that should be less complicated */
 
-// Page elements functionality
-var characterAmountNumber = document.getElementById("characterAmountNumber");
-var characterAmountRange = document.getElementById("characterAmountRange");
+// function writePassword(){
+//     var input = prompt("What number?");
+//     var length = parseInt(input);
+//     function makeid(length) {
+//         var result           = "";
+//         var lower            = "abcdefghijklmnopqrstuvwxyz";
+//         var upper            = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//         var numbers          = "0123456789";
+//         var characters       = lower+upper+numbers;
+//         var charactersLength = characters.length;
+//         for ( var i = 0; i < length; i++ ) {
+//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//         }
+//         return result;
+// }
+// var password = makeid(length);
+// var passwordText = document.querySelector("#password");
+// passwordText.value = password;
+// }
 
-characterAmountRange.addEventListener("input", syncCharacterAmount);
-characterAmountNumber.addEventListener("input", syncCharacterAmount);
 
-function syncCharacterAmount(i) {
-	var value = i.target.value;
-	characterAmountNumber.value = value;
-	characterAmountRange.value = value;
-}
+
+
+
+
+//** Page elements functionality - Stopped to redirect purpose of HW assignment */
+
+// var characterAmountNumber = document.getElementById("characterAmountNumber");
+// var characterAmountRange = document.getElementById("characterAmountRange");
+
+// characterAmountRange.addEventListener("input", syncCharacterAmount);
+// characterAmountNumber.addEventListener("input", syncCharacterAmount);
+
+// function syncCharacterAmount(i) {
+// 	var value = i.target.value;
+// 	characterAmountNumber.value = value;
+// 	characterAmountRange.value = value;
+// }
